a3f3ad7674f46137ce0d129d050b61ae29832500
juju4 <juju4@users.noreply.github.com>
2018-05-05 18:15:49 -0400

    review permission checks with powershell and icacls

--- controls/stig_access.rb
+++ controls/stig_access.rb
@@ -1,6 +1,5 @@
 
 title 'Windows Access Configuration - STIG'
-# TODO: go deeper? Get-Acl -Path ... | fl
 
 control 'windows-acl-100' do
   impact 1.0
@@ -8,8 +7,22 @@ control 'windows-acl-100' do
   desc 'Ensure Windows folder is writable by Administrators but not Users'
   describe file('c:/windows') do
     it { should be_directory }
-    it { should be_writable.by('Administrator') }
-    it { should_not be_writable.by('Users') }
+## FIXME! `check_file_permission_by_mask` is not supported on Windows
+#    it { should be_writable.by('Administrator') }
+#    it { should_not be_writable.by('Users') }
+  end
+  describe powershell('Get-Acl -Path "C:\windows" | fl') do
+    its('stdout') { should include 'BUILTIN\Administrators Allow  268435546' }
+    its('stdout') { should include 'BUILTIN\Users Allow  -1610612736' }
+  end
+  describe command('icacls "c:\Windows"') do
+    its('stdout') { should include 'NT AUTHORITY\SYSTEM:(M)' }
+    its('stdout') { should include 'NT AUTHORITY\SYSTEM:(OI)(CI)(IO)(F)' }
+    its('stdout') { should include 'BUILTIN\Administrators:(M)' }
+    its('stdout') { should include 'BUILTIN\Administrators:(OI)(CI)(IO)(F)' }
+    its('stdout') { should include 'BUILTIN\Users:(RX)' }
+    its('stdout') { should include 'BUILTIN\Users:(OI)(CI)(IO)(GR,GE)' }
+    its('stdout') { should include 'CREATOR OWNER:(OI)(CI)(IO)(F)' }
   end
 end
 
@@ -20,9 +33,19 @@ control 'windows-acl-101' do
   ref url: 'https://www.stigviewer.com/stig/windows_10/2016-11-03/finding/V-63373'
   describe file('c:') do
     it { should be_directory }
-    it { should be_writable.by('Administrator') }
-    it { should be_writable.by('SYSTEM') }
-    it { should_not be_writable.by('Users') }
+## FIXME! `check_file_permission_by_mask` is not supported on Windows
+#    it { should be_writable.by('Administrator') }
+#    it { should be_writable.by('SYSTEM') }
+#    it { should_not be_writable.by('Users') }
+  end
+  describe command('icacls "c:\"') do
+    its('stdout') { should include 'NT AUTHORITY\Authenticated Users:(OI)(CI)(IO)(M)' }
+    its('stdout') { should include 'NT AUTHORITY\SYSTEM:(OI)(CI)(F)' }
+    its('stdout') { should include 'BUILTIN\Administrators:(OI)(CI)(F)' }
+    its('stdout') { should include 'BUILTIN\Users:(OI)(CI)(RX)' }
+    its('stdout') { should include 'BUILTIN\Users:(CI)(AD)' }
+    its('stdout') { should include 'BUILTIN\Users:(CI)(IO)(WD)' }
+    its('stdout') { should include 'CREATOR OWNER:(OI)(CI)(IO)(F)' }
   end
 end
 
@@ -31,27 +54,37 @@ control 'windows-acl-200' do
   title 'Verify the registry permissions are properly set - HKLM'
   desc 'The registry is integral to the function, security, and stability of the Windows system. Changing the system\'s registry permissions allows the possibility of unauthorized and anonymous modification to the operating system.'
   ref url: 'https://www.stigviewer.com/stig/windows_10/2015-11-30/finding/V-63593'
-  describe registry_key('HKLM\Security') do
-    it { should exist }
-    it { should be_writable.by('Administrator') }
-    it { should be_writable.by('SYSTEM') }
-    it { should_not be_readable.by('Users') }
-    it { should_not be_writable.by('Users') }
+#  describe registry_key('HKLM\Security') do
+#    it { should exist }
+#    it { should be_writable.by('Administrator') }
+#    it { should be_writable.by('SYSTEM') }
+#    it { should_not be_readable.by('Users') }
+#    it { should_not be_writable.by('Users') }
+#  end
+#  describe registry_key('HKLM\System') do
+#    it { should exist }
+#    it { should be_writable.by('Administrator') }
+#    it { should be_writable.by('SYSTEM') }
+#    it { should be_writable.by('CREATOR OWNER') }
+#    it { should be_readable.by('Users') }
+#    it { should_not be_writable.by('Users') }
+#  end
+  describe powershell('Get-Acl "HKLM:\System" | fl') do
+    its('stdout') { should include 'BUILTIN\Administrators Allow  FullControl' }
+    its('stdout') { should include 'BUILTIN\Administrators Allow  268435546' }
+    its('stdout') { should include 'BUILTIN\Users Allow  ReadKey' }
+    its('stdout') { should include 'BUILTIN\Users Allow  -2147483648' }
   end
-  describe registry_key('HKLM\System') do
-    it { should exist }
-    it { should be_writable.by('Administrator') }
-    it { should be_writable.by('SYSTEM') }
-    it { should be_writable.by('CREATOR OWNER') }
-    it { should be_readable.by('Users') }
-    it { should_not be_writable.by('Users') }
-  end
-  describe registry_key('HKLM\Software') do
-    it { should exist }
-    it { should be_writable.by('Administrator') }
-    it { should be_writable.by('SYSTEM') }
-    it { should be_writable.by('CREATOR OWNER') }
-    it { should be_readable.by('Users') }
-    it { should_not be_writable.by('Users') }
+#  describe registry_key('HKLM\Software') do
+#    it { should exist }
+#    it { should be_writable.by('Administrator') }
+#    it { should be_writable.by('SYSTEM') }
+#    it { should be_writable.by('CREATOR OWNER') }
+#    it { should be_readable.by('Users') }
+#    it { should_not be_writable.by('Users') }
+#  end
+  describe powershell('Get-Acl "HKLM:\Software" | fl') do
+    its('stdout') { should include 'BUILTIN\Administrators Allow  FullControl' }
+    its('stdout') { should include 'BUILTIN\Users Allow  ReadKey' }
   end
 end
