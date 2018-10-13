42c22880b41ebbedbf5d54ea481c607a199b7928
juju4 <juju4@users.noreply.github.com>
2018-10-13 14:23:16 -0400

    WSH ref urls

--- controls/windows_script_host.rb
+++ controls/windows_script_host.rb
@@ -5,7 +5,9 @@ control 'wsh-101' do
   impact 1.0
   title 'Windows Script Host mitigations'
   desc 'Ensure wscript is disabled'
+  ref url: 'https://technet.microsoft.com/en-ca/library/ee198684.aspx'
   ref url: 'https://labsblog.f-secure.com/2016/04/19/how-to-disable-windows-script-host/'
+  ref url: 'https://isc.sans.edu/forums/diary/Controlling+JavaScript+Malware+Before+it+Runs/21171/'
   describe registry_key('HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings') do
     it { should exist }
     its('Enabled') { should eq 0 }
